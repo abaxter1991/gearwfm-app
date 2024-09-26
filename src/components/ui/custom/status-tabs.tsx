0ecb322d9f5e33d91f8241f495a38f543331386a
Austin Baxter <austin.bryce1991@icloud.com>
2024-09-26 02:31:31 -0600

    chore: updated styles

--- src/components/ui/custom/status-tabs.tsx
+++ src/components/ui/custom/status-tabs.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Tabs, Tab } from '@nextui-org/react'
+import { Card, CardBody, Tabs, Tab } from '@nextui-org/react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useCallback } from 'react'
 
@@ -16,27 +16,53 @@ export function StatusTabs() {
     }, [searchParams])
 
     return (
-        <div className="flex flex-wrap gap-4">
-            <Tabs
-                defaultSelectedKey={searchParams.get('status') || undefined}
-                onSelectionChange={(key) => {
-                    handleTabSelected('status', String(key))
-                }}
-                classNames={{
-                    cursor: [
-                        'group-data-[selected=true]:bg-gradient-to-br',
-                        'group-data-[selected=true]:from-brand-primary',
-                        'group-data-[selected=true]:to-cyan-400',
-                    ],
-                    tabContent: "group-data-[selected=true]:text-black",
-                }}
-            >
-                <Tab key="all" title="All"/>
-                <Tab key="draft" title="Draft"/>
-                <Tab key="pending" title="Pending"/>
-                <Tab key="in_progress" title="In Progress"/>
-                <Tab key="completed" title="Completed"/>
-            </Tabs>
-        </div>
+        <Card className="w-fit">
+            <CardBody>
+                <Tabs
+                    defaultSelectedKey={searchParams.get('status') || undefined}
+                    onSelectionChange={(key) => {
+                        handleTabSelected('status', String(key))
+                    }}
+                    classNames={{
+                        tabList: 'shadow-inner',
+                        cursor: [
+                            'group-data-[selected=true]:bg-gradient-to-br',
+                            'group-data-[selected=true]:from-brand-primary',
+                            'group-data-[selected=true]:to-cyan-400',
+                        ],
+                        tabContent: 'group-data-[selected=true]:text-black',
+                    }}
+                >
+                    <Tab key="all" title="All"/>
+                    <Tab key="draft" title="Draft"/>
+                    <Tab key="pending" title="Pending"/>
+                    <Tab key="in_progress" title="In Progress"/>
+                    <Tab key="completed" title="Completed"/>
+                </Tabs>
+            </CardBody>
+        </Card>
+        // <div className="flex flex-wrap gap-4">
+        //     <Tabs
+        //         defaultSelectedKey={searchParams.get('status') || undefined}
+        //         onSelectionChange={(key) => {
+        //             handleTabSelected('status', String(key))
+        //         }}
+        //         classNames={{
+        //             tabList: 'shadow-inner',
+        //             cursor: [
+        //                 'group-data-[selected=true]:bg-gradient-to-br',
+        //                 'group-data-[selected=true]:from-brand-primary',
+        //                 'group-data-[selected=true]:to-cyan-400',
+        //             ],
+        //             tabContent: 'group-data-[selected=true]:text-black',
+        //         }}
+        //     >
+        //         <Tab key="all" title="All"/>
+        //         <Tab key="draft" title="Draft"/>
+        //         <Tab key="pending" title="Pending"/>
+        //         <Tab key="in_progress" title="In Progress"/>
+        //         <Tab key="completed" title="Completed"/>
+        //     </Tabs>
+        // </div>
     )
 }
