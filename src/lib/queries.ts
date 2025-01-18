e270a8fde43351cf3cde3a44183da737816f1aa1
abaxter1991 <austin.bryce1991@icloud.com>
2025-01-17 18:08:02 -0700

    fix: revalidation has been removed

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,11 +1,11 @@
-import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import type { SalesOrderProduct } from '@prisma/client'
 import type { SalesOrderAndRelations } from '~/types'
 
 export function useSalesOrder(salesOrderId: string | null | undefined) {
-    return useSWR<SalesOrderAndRelations>(`/sales-order/${salesOrderId}`)
+    return useSWRImmutable<SalesOrderAndRelations>(`/sales-order/${salesOrderId}`)
 }
 
 export function useSalesOrderProduct(productId: string | null | undefined) {
-    return useSWR<SalesOrderProduct>(`/product/${productId}`)
+    return useSWRImmutable<SalesOrderProduct>(`/product/${productId}`)
 }
